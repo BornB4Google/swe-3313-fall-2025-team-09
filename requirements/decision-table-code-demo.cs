28b171a93c2f893ba9608b777dfb423a96794c68
emmyatri <aelling6@students.kennesaw.edu>
2025-10-22 14:27:42 -0400

    update decision table

---
+++ requirements/decision-table-code-demo.cs
@@ -0,0 +1,53 @@
+public class decisionTable
+{
+
+    public static void Main()
+    {
+
+
+
+    }
+
+    public class Inventory
+    {
+        List<string> items = new List<string>();
+        
+
+
+
+
+
+    }
+    
+    public class User
+    {
+        List<string> cart = new List<string>();
+        
+        public void login()
+        {
+
+        }
+
+        public void searchInventory()
+        {
+
+        }
+
+        public void addItem()
+        {
+
+        }
+        
+        
+    }
+    
+    public class Admin : User
+    {
+        
+    }
+
+
+
+
+
+}
---
+++ requirements/decision-table.md.md
@@ -0,0 +1,3 @@
+# Link to Google Doc with decision table
+
+https://docs.google.com/document/d/1usihAPugGA74EIkEqDj90BOS_rVaAgaGLXYMohW-QBc/edit?usp=sharing
