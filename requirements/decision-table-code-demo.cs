0b2ea8eb89601e850d7a2fb1291c9700b6068180
atressler5 <atressler5@gmail.com>
2025-10-23 02:56:33 -0400

    Started implementation of demo

--- requirements/decision-table-code-demo.cs
+++ requirements/decision-table-code-demo.cs
@@ -1,4 +1,4 @@
-public class decisionTable
+public class DecisionTable
 {
 
     public static void Main()
@@ -8,46 +8,170 @@ public class decisionTable
 
     }
 
-    public class Inventory
+    private static void AddTestItems()
     {
-        List<string> items = new List<string>();
         
-
-
-
-
-
     }
+
     
     public class User
     {
-        List<string> cart = new List<string>();
+        List<Item> cart = new List<Item>();
+        private Inventory storesInventory;
         
-        public void login()
+        public User(Inventory inventory)
         {
-
+            storesInventory = inventory;
+            Login();
+        }
+        public void Login()
+        {
+            Console.WriteLine("Give us your money!");
         }
 
-        public void searchInventory()
+        //search if any item has the same name as the item you're looking for
+        public Item SearchInventory(string s)
         {
-
+            foreach (Item i in storesInventory)
+            {
+                if (i.name == s) return i;
+            }
+            return null;
         }
 
-        public void addItem()
+        public void AddItemToCart(string itemName)
         {
+            //tuple of item and quantity
+            var item = SearchInventory(itemName);
+            if (item == null)
+            {
+                Console.WriteLine("No such item in inventory. Cannot add to cart.");
+                return;
+            }
+            else if (item.quantity < 1)
+            {
+                Console.WriteLine("Item is out of stock.");
+                return;
+            }
+            else
+            {
+                //making seperate object so changes on the object in cart don't affect object in inventory
+                Item cartItem = new Item(itemName, 1);
+                cart.Add(cartItem);
+                item.quantity--;
+                Console.WriteLine("Added item " + itemName + "to cart.");
+            }
+        }
 
+        public void RemoveItemFromCart(string itemName)
+        {
+            var item = SearchInventory(itemName);
+
+            if (item == null)
+            {
+                Console.WriteLine("No such item in cart. Cannot add to cart.");
+                return;
+            }
+            else
+            {
+                cart.Remove(item);
+                var.quantity++;
+                Console.WriteLine("Removed item " + itemName + "to cart.");
+            }
+        }
+        
+        private Item SearchCart(string itemName){
+            foreach (Item i in cart)
+            {
+                if (i.name == itemName) return i;
+            }
+            return null;
+        }
+        
+        public void Checkout()
+        {
+            cart = null;
+            Console.WriteLine("Money, money, money, must be funny \n In the rich man's world");
         }
         
         
     }
-    
+
+
+
+
     public class Admin : User
     {
-        
+        public Admin(Inventory inventory)
+        {
+            storeInventory = inventory;
+        }
+
+        public override void Login()
+        {
+            Console.WriteLine("Wa wa wee wa! King in the castle, king in the castle!");
+        }
+
+        //Don't need to override SearchInventory
+
+        public override void AddItemToCart(string itemName)
+        {
+            Console.WriteLine("Admins cannot add items to cart.");
+        }
+
+        public override void RemoveItemFromCart(string itemName)
+        {
+            Console.WriteLine("Admins cannot remove items from cart.");
+        }
+
+        //Shouldn't need since private and no other methods call, just here in case
+        private override void SearchCart(string itemName)
+        { }
+
+        public override void Checkout()
+        {
+            Console.WriteLine("Thief!");
+        }
+
+        public void AddToInventory(string itemName, int quantity)
+        {
+            var item = SearchInventory(itemName);
+
+            if (item != null)
+            {
+                item.quantity += quantity;
+            }
+            else
+            {
+                item i = new Item();
+                storesInventory.add
+            }
+
+
+        }
     }
 
 
 
+    public class Inventory
+    {
+        List<Item> items = new List<Item>();
+        public Inventory()
+        {
+        }
+    }
 
+    public class Item
+    {
+        public string name;
+        public int quantity;
 
+        public Item(string name, int price, int quantity)
+        {
+            this.name = name;
+            this.price = price;
+            this.quantity = quantity;
+        }
+    }
+    
 }
