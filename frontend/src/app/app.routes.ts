72cc3dd8d71577174dab22e6d47c45019cb709ce
missblankie <swaymen@students.kennesaw.edu>
2025-11-11 14:28:40 -0500

    updated frontend app

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
-
+import {InventoryComponent } from './inventory/inventory.component';
+import {CheckoutComponent } from './checkout/checkout.component';
 export const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
+  { path: 'inventory', component: InventoryComponent },
+  { path: 'checkout', component: CheckoutComponent },
   { path: '**', redirectTo: '' },
 ];
