1f8e1509edda95ee6603e5b5979f6f71afab4509
emmyatri <aelling6@students.kennesaw.edu>
2025-11-24 14:44:27 -0500

    add method to return users information for account page

--- source/Controllers/UsersController.cs
+++ source/Controllers/UsersController.cs
@@ -17,8 +17,39 @@ public class UsersController : ControllerBase
         _db = db;
     }
     
+    
+    //GET /api/users/me
+    // returns current logged-n user's profile
+    [HttpGet("me")]
+    [Authorize]
+    public async Task<IActionResult> GetCurrentUser()
+    {
+        var userIdClaim = User.FindFirst("UserId")?.Value;
+        if (userIdClaim == null || !int.TryParse(userIdClaim, out var userId))
+            return Unauthorized();
+        
+        var user = await _db.Users
+            .Where(u => u.UserId == userId)
+            .Select (u => new 
+                {
+                u.Username,
+                u.UserId,
+                u.FirstName,
+                u.LastName,
+                u.Email,
+                
+                })
+            .FirstOrDefaultAsync();
+        if (user == null)
+            return NotFound();
+        return Ok(user);
+    }
+    
+    
+    
+    
     // PUT /api/users/{id}/role
-    // Admin only.. change a user to an admin
+    // Admin only. change a user to an admin
     [HttpPut("{id:int}/role")]
     [Authorize(Roles = "Admin")]
     public async Task<IActionResult> UpdateRole(int id, [FromBody] ChangeUserRoleRequest request)
