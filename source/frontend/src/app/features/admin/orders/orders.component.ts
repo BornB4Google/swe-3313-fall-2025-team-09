dc4da3be748fcb1975458a683c8b6fd5ad750697
missblankie <swaymen@students.kennesaw.edu>
2025-11-25 20:40:32 -0500

    feat update order
    - implementing api
    
    Currently has the following error
    core.mjs:20842 Angular is running in development mode.
    app.component.ts:29 URL: /admin/orders Landing: false Login: false Signup: false
    :5243/api/orders:1  Failed to load resource: the server responded with a status of 500 (Internal Server Error)
    orders.component.ts:28 Failed to load sales: HttpErrorResponse
    error @ orders.component.ts:28

--- source/frontend/src/app/features/admin/orders/orders.component.ts
+++ source/frontend/src/app/features/admin/orders/orders.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
-import {NgForOf} from '@angular/common';
+import {Component, OnInit} from '@angular/core';
+import {CurrencyPipe, DatePipe, NgForOf, NgIf} from '@angular/common';
+import {InventoryItem} from '../../../models/inventory.models';
+import {RouterLink} from '@angular/router';
+import { Sale } from '../../../models/sale.model';
+import { SaleService } from '../../../services/sales/sale.service';
 
 @Component({
   selector: 'app-orders',
   imports: [
-    NgForOf
+    NgForOf,
+    CurrencyPipe,
+    NgIf,
+    RouterLink,
+    DatePipe
   ],
   templateUrl: './orders.component.html',
   styleUrl: './orders.component.css'
 })
-export class OrdersComponent {
+export class OrdersComponent implements OnInit  {
 
+  receipts: Sale[] = [];
+  selectedReceipt: Sale | null = null;
+  constructor(private saleService: SaleService) {}
+  ngOnInit(): void {
+    this.saleService.getAllOrders().subscribe({
+      next: (sales) => this.receipts = sales,
+      error: (err) => console.error('Failed to load sales:', err)
+    });
+  }
+
+
+  openReceipt(receipt: Sale) {
+    this.selectedReceipt = receipt;
+  }
+
+  closeReceipt() {
+    this.selectedReceipt = null;
+  }
 }
---
+++ source/frontend/src/app/services/sales/sale.service.ts
@@ -0,0 +1,23 @@
+import { inject, Injectable } from '@angular/core';
+import { User } from '../../models/user.models';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Sale } from '../../models/sale.model';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SaleService {
+  private http = inject(HttpClient);
+  constructor() { }
+
+  getAllOrders(): Observable<Sale[]> {
+    return this.http.get<Sale[]>('/api/orders');
+  }
+
+  getOrderById(id: number): Observable<Sale> {
+    return this.http.get<Sale>(`/api/orders/${id}`);
+  }
+
+}
