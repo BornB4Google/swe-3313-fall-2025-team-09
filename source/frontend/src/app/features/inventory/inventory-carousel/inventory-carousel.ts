79301b73be21da04470e365e794eea658dff1fe9
atressler5 <atressler5@gmail.com>
2025-12-02 17:22:08 -0500

    Update inventory-carousel.ts

--- source/frontend/src/app/features/inventory/inventory-carousel/inventory-carousel.ts
+++ source/frontend/src/app/features/inventory/inventory-carousel/inventory-carousel.ts
@@ -1,12 +1,36 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, Input, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InventoryImage } from '../../../models/inventory.models';
 
 @Component({
   selector: 'app-inventory-carousel',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './inventory-carousel.component.html',
   styleUrls: ['./inventory-carousel.component.css']
 })
 export class InventoryCarouselComponent {
-  constructor() {}
+
+  //sets a required parameter - an array of InventoryImages
+  @Input({ required: true }) images: InventoryImage[] = [];
+
+  //signal so re-rendered automatically when changed
+  currentIndex = signal(0);
+
+  //use mod to wrap around to start of array
+  next(): void {
+    const n = this.images.length;
+    if (n === 0) return;
+    this.currentIndex.update(i => (i + 1) % n);
+  }
+
+  prev(): void {
+    const n = this.images.length;
+    if (n === 0) return;
+    this.currentIndex.update(i => (i - 1 + n) % n);
+  }
+
+
 
 
 }
