d95e6abcdb2d7a527e9c74962248a7977f474227
atressler5 <atressler5@gmail.com>
2025-12-02 16:34:19 -0500

    made base files for image-carousel component

---
+++ source/frontend/src/app/features/inventory/inventory-carousel/inventory-carousel.ts
@@ -0,0 +1,12 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inventory-carousel',
+  templateUrl: './inventory-carousel.component.html',
+  styleUrls: ['./inventory-carousel.component.css']
+})
+export class InventoryCarouselComponent {
+  constructor() {}
+
+
+}
