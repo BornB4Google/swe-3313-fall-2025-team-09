a95ebb370611dee3cf5a1fbc3c55c1a909231a53
atressler5 <atressler5@gmail.com>
2025-12-04 20:59:58 -0500

    added arrows to buttons

--- source/frontend/src/app/features/inventory/inventory-carousel/inventory-carousel.component.ts
+++ source/frontend/src/app/features/inventory/inventory-carousel/inventory-carousel.component.ts
@@ -1,16 +1,15 @@
 ﻿import { Component, Input, signal, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { InventoryItem } from '../../../models/inventory.models';
 
 @Component({
   selector: 'app-inventory-carousel',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgOptimizedImage],
   templateUrl: './inventory-carousel.component.html',
-  styleUrls: ['./inventory-carousel.component.css']
+  styleUrls: ['./inventory-carousel.component.css'],
 })
 export class InventoryCarouselComponent implements OnInit {
-
   //sets a required parameter - an InventoryItem
   @Input({ required: true }) item!: InventoryItem;
 
@@ -36,9 +35,8 @@ export class InventoryCarouselComponent implements OnInit {
     //PrimaryPhotoUrl always index 0 in the array imageUrls
     this.imageUrls.push(this.item.primaryPhotoUrl);
     //add rest of urls from the array images as described in inventory.models.ts
-    for(const image of this.item.images){
+    for (const image of this.item.images) {
       this.imageUrls.push(image.imageUrl);
     }
   }
-
 }
