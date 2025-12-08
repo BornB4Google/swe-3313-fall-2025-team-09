a07d9e57e3266733d7ab9747e784a5e042204e94
Amy Ward <amyward@amys-mbp.lan>
2025-12-08 13:36:35 -0500

    fix whitespace

--- source/DTOs/OrdersDTOs.cs
+++ source/DTOs/OrdersDTOs.cs
@@ -56,8 +56,7 @@ public class OrderDetailDto
 // Request body for POST /api/orders/checkout
 public class CheckoutRequestDto
 {
-    [Required]
-    [StringLength(20)] public string ShippingSpeed {get; set; } = string.Empty;
+    [Required] [StringLength(20)] public string ShippingSpeed { get; set; } = string.Empty;
 
     [Required]
     [StringLength(100, MinimumLength = 3, ErrorMessage = "Street1 must be at least 3 characters.")]
