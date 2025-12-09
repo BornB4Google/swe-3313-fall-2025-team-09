dc021dfbf99f2656f0239f787e3742798a5e0e2f
atressler5 <atressler5@gmail.com>
2025-12-08 22:56:37 -0500

    fix images staying the same when selecting different items

--- source/frontend/src/app/features/inventory/inventory-carousel/inventory-carousel.component.ts
+++ source/frontend/src/app/features/inventory/inventory-carousel/inventory-carousel.component.ts
@@ -1,15 +1,15 @@
-﻿import { Component, Input, signal, OnInit } from '@angular/core';
-import { CommonModule, NgOptimizedImage } from '@angular/common';
+﻿import { Component, Input, signal,SimpleChanges, OnChanges } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { InventoryItem } from '../../../models/inventory.models';
 
 @Component({
   selector: 'app-inventory-carousel',
   standalone: true,
-  imports: [CommonModule, NgOptimizedImage],
+  imports: [CommonModule],
   templateUrl: './inventory-carousel.component.html',
   styleUrls: ['./inventory-carousel.component.css'],
 })
-export class InventoryCarouselComponent implements OnInit {
+export class InventoryCarouselComponent implements OnChanges {
   //sets a required parameter - an InventoryItem
   @Input({ required: true }) item!: InventoryItem;
 
@@ -31,12 +31,24 @@ export class InventoryCarouselComponent implements OnInit {
     this.currentIndex.update(i => (i - 1 + n) % n);
   }
 
-  ngOnInit() {
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['item'] && this.item) {
+      this.changeUrls();
+    }
+  }
+
+  private changeUrls(): void {
+    //clear then add new images so doesn't keep old ones
+    this.imageUrls = [];
+    //set to 0
+    this.currentIndex.update(i => i - i);
     //PrimaryPhotoUrl always index 0 in the array imageUrls
     this.imageUrls.push(this.item.primaryPhotoUrl);
     //add rest of urls from the array images as described in inventory.models.ts
     for (const image of this.item.images) {
       this.imageUrls.push(image.imageUrl);
     }
+
   }
+
 }
