18b5a756bc9335abdd0e79aa0e367ee633a33808
emmyatri <aelling6@students.kennesaw.edu>
2025-12-09 14:27:39 -0500

    trigger rebuild

---
+++ source/Migrations/20251209192719_AddCustomerNameToSale.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCustomerNameToSale : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CustomerName",
+                table: "Sales",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomerName",
+                table: "Sales");
+        }
+    }
+}
