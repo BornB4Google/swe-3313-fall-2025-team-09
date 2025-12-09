35645c43dd0ca01373d511f16ae76df55d111eb2
Amy Ward <amyward@Amys-MacBook-Pro.local>
2025-12-09 13:42:36 -0500

    add migrations

---
+++ source/Migrations/20251209184207_AddSaleContactFields.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSaleContactFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Sales",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Sales",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Sales",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Sales");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Sales");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Sales");
+        }
+    }
+}
