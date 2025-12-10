25c087157b974c6988ae273c5408f99209e1c3a5
Isaac Thoman <49598528+IsaacThoman@users.noreply.github.com>
2025-12-09 19:08:25 -0500

    style: auto fix build errors

--- source/DTOs/CartDTOs.cs
+++ source/DTOs/CartDTOs.cs
@@ -15,7 +15,7 @@ public class CartItemDto
     public string Name { get; set; } = string.Empty;
     public string Category { get; set; } = string.Empty;
 
-    public string Description { get; set; }
+    public string Description { get; set; } = string.Empty;
 
     public decimal UnitPrice { get; set; }
 
